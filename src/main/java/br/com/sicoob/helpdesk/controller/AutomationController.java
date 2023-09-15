3e8012d3265f5baa7e49354b08fa1299d70a178b
GabsLopes055 <lopesgabriel055@gmail.com>
2023-09-15 17:39:48 -0300

    Criado os metodos de listar e buscar uma bat

--- src/main/java/br/com/sicoob/helpdesk/controller/AutomationController.java
+++ src/main/java/br/com/sicoob/helpdesk/controller/AutomationController.java
@@ -2,6 +2,7 @@ package br.com.sicoob.helpdesk.controller;
 
 import br.com.sicoob.helpdesk.dto.response.AutomationsResponse;
 import br.com.sicoob.helpdesk.service.AutomationService;
+import jakarta.persistence.Lob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/sat/automation")
@@ -21,6 +23,18 @@ public class AutomationController {
     @CrossOrigin(value = "*")
     public ResponseEntity<AutomationsResponse>  saveNewAutomation(@RequestParam(value = "file") MultipartFile file) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.createdAutomation(file));
+    }
+
+    @GetMapping
+    @CrossOrigin(value = "*")
+    public ResponseEntity<List<AutomationsResponse>> listAllAutomation() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.listAllAutomations());
+    }
+
+    @GetMapping(value = "{id}")
+    @CrossOrigin(value = "*")
+    public ResponseEntity<AutomationsResponse> findOneAutomation(@PathVariable(value = "id")Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findOneAutomation(id));
     }
 
 }
--- src/main/java/br/com/sicoob/helpdesk/service/AutomationService.java
+++ src/main/java/br/com/sicoob/helpdesk/service/AutomationService.java
@@ -3,12 +3,16 @@ package br.com.sicoob.helpdesk.service;
 import br.com.sicoob.helpdesk.dto.response.AutomationsResponse;
 import br.com.sicoob.helpdesk.entities.Automations;
 import br.com.sicoob.helpdesk.repository.AutomationsRepository;
+import br.com.sicoob.helpdesk.service.exceptions.EntityNotFoundException;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AutomationService {
@@ -26,6 +30,33 @@ public class AutomationService {
         automation.setData(file.getBytes());
 
         return AutomationsResponse.automationsResponse(repository.save(automation));
+
+    }
+
+    //metodo para listar todas as bats
+    public List<AutomationsResponse> listAllAutomations() {
+        List<Automations> listAll = repository.findAll();
+
+        return listAll.stream().map(AutomationsResponse::automationsResponse).collect(Collectors.toList());
+    }
+
+    //metodo para listar uma bat por id
+    public AutomationsResponse findOneAutomation(Long id) {
+
+       Optional<Automations> findAutomation = Optional.ofNullable(repository.findById(id).orElseThrow(
+               () -> new EntityNotFoundException("id não encontrado")
+       ));
+
+       var automation = findAutomation.get();
+
+       AutomationsResponse response = new AutomationsResponse();
+
+       response.setId(automation.getCdAutomation());
+       response.setDocName(automation.getDocName());
+       response.setDocType(automation.getDocType());
+       response.setData(automation.getData());
+
+       return response;
 
     }
 
