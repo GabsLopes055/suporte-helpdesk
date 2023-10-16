91b505b0e23a3e4ca3f22365afabc05727429c3c
GabsLopes055 <lopesgabriel055@gmail.com>
2023-10-16 17:57:19 -0300

    Alterando a listagem das reservas para listar a mais recente'

--- src/main/java/br/com/sicoob/helpdesk/repository/ReserveOfSecondFloorRepository.java
+++ src/main/java/br/com/sicoob/helpdesk/repository/ReserveOfSecondFloorRepository.java
@@ -2,8 +2,15 @@ package br.com.sicoob.helpdesk.repository;
 
 import br.com.sicoob.helpdesk.entities.ReserveOfSecondFloorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReserveOfSecondFloorRepository extends JpaRepository<ReserveOfSecondFloorEntity, Long> {
+
+    @Query(value = "SELECT * FROM tbl_reserve_of_second_floor ORDER BY cd_reserve DESC LIMIT 25", nativeQuery = true)
+    List<ReserveOfSecondFloorEntity> findAll();
+
 }
